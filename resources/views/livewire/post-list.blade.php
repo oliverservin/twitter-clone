ec1ebde5e294484cc61db1d2d98c3966cad97197
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-13 12:47:03 -0600

    wip (#5)

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -13,6 +13,12 @@ state(['posts' => $getPosts]);
 
 on(['post-created' => $getPosts]);
 
+$toggleLike = function (Post $post) {
+    auth()->user()->likedPosts()->toggle($post);
+
+    $this->getPosts();
+}
+
 ?>
 
 <div>
@@ -36,6 +42,23 @@ on(['post-created' => $getPosts]);
                     {{ $post->body }}
                 </div>
                 <div class="mt-3 flex flex-row items-center gap-10">
+                    <button
+                        @auth
+                            wire:click="toggleLike({{ $post->id }})"
+                        @else
+                            @click="$dispatch('show-login-modal')"
+                        @endauth
+                        class="relative z-10 flex flex-row items-center gap-2 text-neutral-500 transition hover:text-red-500"
+                    >
+                        @if ($post->likedBy->contains(auth()->user()))
+                            <x-icon.heart-solid class="size-5 text-red-500" />
+                        @else
+                            <x-icon.heart-outline class="size-5" />
+                        @endif
+                        <p>
+                            {{ $post->likedBy()->count() }}
+                        </p>
+                    </button>
                     <a
                         href="{{ route('posts.show', ['post' => $post]) }}" wire:navigate
                         class="relative z-10 flex flex-row items-center gap-2 text-neutral-500 transition hover:text-sky-500"
--- resources/views/pages/posts/[Post].blade.php
+++ resources/views/pages/posts/[Post].blade.php
@@ -8,27 +8,7 @@ name('posts.show');
 
 <x-layouts.app>
     <x-header with-back-button>Post</x-header>
-    <div class="relative border-b-[1px] border-neutral-800 p-5 transition hover:bg-neutral-900">
-        <div class="flex flex-row items-start gap-3">
-            <x-avatar :user="$post->user" />
-            <div>
-                <div class="flex flex-row items-center gap-2">
-                    <span class="relative z-10 font-semibold text-white hover:underline">
-                        {{ $post->user->name }}
-                    </span>
-                    <span class="relative z-10 hidden text-neutral-500 hover:underline md:block">
-                        {{ '@'.$post->user->username }}
-                    </span>
-                    <span class="text-sm text-neutral-500">
-                        {{ $post->created_at->diffForHumans() }}
-                    </span>
-                </div>
-                <div class="mt-1 text-white">
-                    {{ $post->body }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <livewire:show-post :post="$post" />
     @auth
         <livewire:comment-form :post="$post" />
     @else
