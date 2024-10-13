ba3b6a40997f8646107fceeca703bd4d5904a255
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-13 12:43:46 -0600

    wip

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Comment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
+        'post_id',
         'body',
     ];
 
@@ -27,8 +29,8 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function comments()
+    public function post()
     {
-        return $this->hasMany(Comment::class);
+        return $this->belongsTo(Post::class);
     }
 }
