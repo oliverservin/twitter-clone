755591c8f924e69f69f6179d9c2b8984dd994c53
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-15 10:34:09 -0600

    wip

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,14 @@
+<svg {{ $attributes }} width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="48" height="48" fill="transparent" />
+    <path d="M8.99786 17.8433L39.9033 0L39.0844 12.6994L12.7638 27.8955L8.99786 17.8433Z" fill="white" />
+    <path
+        opacity="0.5"
+        d="M12.7637 27.8948L34.4994 15.3457L33.6805 28.0451L16.5296 37.9471L12.7637 27.8948Z"
+        fill="white"
+    />
+    <path
+        opacity="0.25"
+        d="M16.5273 37.9477L29.0933 30.6927L28.2744 43.3921L20.2933 48L16.5273 37.9477Z"
+        fill="white"
+    />
+</svg>
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,31 @@
+<div class="col-span-1 h-full pr-4 md:pr-6">
+    <div class="flex flex-col items-end">
+        <div class="space-y-2 lg:w-[230px]">
+            <a
+                href="{{ route('home') }}"
+                wire:navigate
+                class="flex h-14 w-14 items-center justify-center rounded-full p-4 hover:bg-blue-300 hover:bg-opacity-10"
+            >
+                <x-logo class="size-7" />
+            </a>
+
+            <a href="#" class="flex flex-row items-center">
+                <div
+                    class="relative flex h-14 w-14 cursor-pointer items-center justify-center rounded-full p-4 hover:bg-slate-300 hover:bg-opacity-10 lg:hidden"
+                >
+                    <!-- icono size-7 -->
+                </div>
+                <div
+                    class="items-row relative hidden cursor-pointer items-center gap-4 rounded-full p-4 hover:bg-slate-300 hover:bg-opacity-10 lg:flex"
+                >
+                    <!-- icono size-6 -->
+                    <p class="hidden text-xl text-white lg:block">Inicio</p>
+                </div>
+            </a>
+
+            <!-- Botón de cerrar sesión -->
+
+            <!-- Botón para publicar -->
+        </div>
+    </div>
+</div>
